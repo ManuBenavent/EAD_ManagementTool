0fc37e1a9258442beaa9c7632b8e2addacc4bb18
ManuBenavent <47591843+ManuBenavent@users.noreply.github.com>
2019-11-21 14:19:25 +0000

    Update Event.cs

--- CW2/TimeManagementAndReportingTool/library/Event.cs
+++ CW2/TimeManagementAndReportingTool/library/Event.cs
@@ -1,1 +1,25 @@
+using System;
 
+namespace library
+{
+    public abstract class Location
+    {
+        public string Name { get; set; }
+        public bool Recurring { get; set; }
+        DAC ddbb;
+        
+        public Event (string Name, bool Recurring)
+        {
+            this.Name = Name;
+            this.Recurring = Recurring;
+            ddbb = new DAC();
+        }
+        
+        public static List<Event> read ()
+        {
+            return ddbb.readEvent();
+        }
+         
+          
+    }
+}
