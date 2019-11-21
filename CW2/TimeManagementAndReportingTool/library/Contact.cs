f9832324e72ce1b22a9fc1a0ba240147f03e3a81
ManuBenavent <47591843+ManuBenavent@users.noreply.github.com>
2019-11-21 14:03:13 +0000

    Update Contact.cs

--- CW2/TimeManagementAndReportingTool/library/Contact.cs
+++ CW2/TimeManagementAndReportingTool/library/Contact.cs
@@ -4,6 +4,43 @@ namespace library
 {
     public class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        DAC ddbb;
         
+        public Contact () {
+            ddbb = new DAC();
+        }
+        
+        public Contact ( string FirstName, string LastName, string Email, string Phone ) {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Phone = Phone;
+            ddbb = new DAC();
+        }
+        
+        public void create() {
+            ddbb.create(this);
+        }
+        
+        public void update() {
+            ddbb.update(this);
+        }
+        
+        public void delete() {
+            ddbb.delete(this)
+        }
+        
+        public void read (int Id) {
+            ddbb.readContact(Id); //TODO review so DRY is applied
+        }
+        
+        public static List<Contact> read () {
+            return ddbb.readContacts()
+        }
+            
     }
 }
