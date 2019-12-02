2fee796e772eb5b0834af191dcdf06cdb1407f02
ManuBenavent <47591843+ManuBenavent@users.noreply.github.com>
2019-12-02 11:58:47 +0000

    Added threading to implemented methods

--- CW2/TimeManagementAndReportingTool/library/DAC.cs
+++ CW2/TimeManagementAndReportingTool/library/DAC.cs
@@ -94,27 +94,40 @@ namespace library
                     throw new DDBBException("GetId");
             }
             string statement = "SELECT Id FROM " + TABLE;
-            // TODO DRY + THREADS
-            SqlConnection c = null;
-            int Id = -1;
+            int Id=-1;
+            Task t = Task.Run( () => {
+                SqlConnection c = null;
+                try
+                {
+                    c = new SqlConnection(constring);
+                    c.Open();
+                    SqlCommand com = new SqlCommand(statement, c);
+                    SqlDataReader reader = com.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        Id = int.Parse(reader["Id"].ToString());
+                    }
+
+                    reader.Close();
+                    if (Id == -1)
+                        throw new DDBBException("GetId: Id not found");
+                }
+                catch (SqlException ex)
+                {
+                    throw new DDBBException("GetId: " + ex.Message);
+                }
+                finally
+                {
+                    c.Close();
+                }
+            });
             try
             {
-                c = new SqlConnection(constring);
-                c.Open();
-                SqlCommand com = new SqlCommand(statement, c);
-                SqlDataReader reader = com.ExecuteReader();
-                if (reader.Read())
-                    Id = int.Parse(reader["Id"].ToString());
-                reader.Close();
-                if (Id == -1)
-                    throw new DDBBException("GetId: Id not found");
-            }catch(SqlException ex)
-            {
-                throw new DDBBException("GetId: " + ex.Message);
+                t.Wait();
             }
-            finally
+            catch (AggregateException ex)
             {
-                c.Close();
+                throw new DDBBException("GetId " + ex.Message);
             }
             return Id;
         }
@@ -208,25 +221,35 @@ namespace library
         /// <param name="statement">The SQL statement to be performed.</param>
         private void SQLNonQuery (string statement)
         {
-            // TODO create thread
-            SqlConnection c = null;
+            Task task = Task.Run(() =>
+            {
+                  SqlConnection c = null;
+                  try
+                  {
+                      c = new SqlConnection(constring);
+                      c.Open();
+
+                      SqlCommand com = new SqlCommand(statement, c);
+
+                      if (com.ExecuteNonQuery() == 0)
+                          throw new DDBBException("SQLNonQuery");
+                  }
+                  catch (SqlException ex)
+                  {
+                      throw new DDBBException("SQLNonQuery " + ex.Message);
+                  }
+                  finally
+                  {
+                      c.Close();
+                  }
+            });
             try
             {
-                c = new SqlConnection(constring);
-                c.Open();
-
-                SqlCommand com = new SqlCommand(statement, c);
-
-                if (com.ExecuteNonQuery() == 0)
-                    throw new DDBBException("SQLNonQuery");
+                task.Wait();
             }
-            catch (SqlException ex)
+            catch( AggregateException ex)
             {
                 throw new DDBBException("SQLNonQuery " + ex.Message);
-            }
-            finally
-            {
-                c.Close();
             }
         }
     }
